e71372f73f20b8579163cdfd03495adca28778d8
FelicsXIII <felics1689@gmail.com>
2023-11-10 14:11:35 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Первый вариант");
         int wallet = 100; // начальный счет
-        int amount = 1001;// пополнение счета
+        int amount = 300;// пополнение счета
         System.out.println("Пополнение: " + amount + " рублей");
         int bonus = amount / 100;
         if (amount < 1000) {
